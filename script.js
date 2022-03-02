78854491d3027d205adaeafac6dae0afa4149384
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-03-02 12:58:31 +0100

    styles and comments

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstati
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-//Copy here your firebase configuration
+//Copy your firebase configuration
 };
 
 // Initialize Firebase
@@ -22,32 +22,35 @@ const db = getFirestore(app);
 const storage = getStorage();
 
 
-const signUpForm = document.getElementById('signup-form')
-const loginForm = document.getElementById('login-form')
-const logout = document.getElementById('logout')
+const signUpForm = document.getElementById('signup-form');
+const loginForm = document.getElementById('login-form');
+const logout = document.getElementById('logout');
+const userData = document.getElementById('user-data');
 
+//SignUp function
 signUpForm.addEventListener('submit', async (e) => {
   e.preventDefault();
-  const signUpEmail = document.getElementById('signup-email').value
-  const signUpPassword = document.getElementById('signup-pass').value
-  const signUpUser = document.getElementById('signup-user').value
+  const signUpEmail = document.getElementById('signup-email').value;
+  const signUpPassword = document.getElementById('signup-pass').value;
+  const signUpUser = document.getElementById('signup-user').value;
   const usersRef = collection(db, "users");
   const signUpImg = document.getElementById('signup-picture').files[0];
   const storageRef = ref(storage, signUpImg.name);
   let publicImageUrl;
   try {
+    //Create auth user
     await createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
     .then((userCredential) => {
-      console.log('Usuario registrado')
-      // Signed in
+      console.log('User registered')
       const user = userCredential.user;
-      // ...
       signUpForm.reset();
     })
+    //Upload file to cloud storage
     await uploadBytes(storageRef, signUpImg).then(async (snapshot) => {
       console.log('Uploaded a blob or file!')
       publicImageUrl= await getDownloadURL(storageRef);
     })
+    //Create document in DB
     await setDoc(doc(usersRef, signUpEmail), {
       username: signUpUser,
       email: signUpEmail,
@@ -58,23 +61,24 @@ signUpForm.addEventListener('submit', async (e) => {
       
 })
 
+//Login function
 loginForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const loginEmail = document.getElementById('login-email').value;
   const loginPassword = document.getElementById('login-pass').value;
+  //Call the collection in the DB
   const docRef = doc(db, "users", loginEmail);
+  //Search a document that matches with our ref
   const docSnap = await getDoc(docRef);
-  const userData = document.getElementById('user-data');
 
   signInWithEmailAndPassword(auth, loginEmail, loginPassword)
     .then((userCredential) => {
-      console.log('Usuario autenticado')
+      console.log('User authenticated')
       const user = userCredential.user;
       loginForm.reset();
     })
     .then(() => {
       if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
         userData.style.cssText = 'background-color: #73AB84;width: 50%;margin: 2rem auto;padding: 1rem;border-radius: 5px;display: flex;flex-direction: column;align-items: center';
         userData.innerHTML = `<h3>User Data</h3>
                               <p>Username: ${docSnap.data().username}</p>
@@ -90,22 +94,22 @@ loginForm.addEventListener('submit', async (e) => {
     });
 })
 
-
+//Logout function
 logout.addEventListener('click', () => {
   signOut(auth).then(() => {
-    console.log('Te has deslogeado')
-    document.getElementById('user-data').innerHTML = ``;
+    console.log('Logout user')
+    userData.style.cssText = '';
+    userData.innerHTML = ``;
   }).catch((error) => {
     console.log('Error: ', error)
   });
 })
 
-const userdata = document.getElementById('user-data')
-
+//Observe the user's state
 auth.onAuthStateChanged(user => {
   if(user){
-    console.log('existo')
+    console.log('Logged user');
   }else{
-    console.log('noexisto');
+    console.log('No logged user');
   }
 })
